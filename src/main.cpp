e30b6b2f9217ed2d79bf7268df228927ff77c14c
Ruthvik Venkatesan <ruthvikotravenkatesan@gmail.com>
2026-05-02 17:23:49 -0400

    lora over uart works

--- src/main.cpp
+++ src/main.cpp
@@ -2,32 +2,59 @@
 #include <twai.h>
 #include <Adafruit_NeoPixel.h>
 
-// The built-in NeoPixel on the ESP32-C6-DevKitC-1 is on GPIO 8
 #define PIN        8 
 #define NUMPIXELS  1 
-
-// Set up the NeoPixel object
+#define INPUT_PIN  15 
 Adafruit_NeoPixel pixels(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
 
 void setup() {
-  // --- NeoPixel Setup ---
-  pixels.begin(); // Initialize the NeoPixel strip object
-  pixels.clear(); // Set all pixel colors to 'off'
-  pixels.setBrightness(50); // Set brightness to ~20% (0-255) so it doesn't blind you
   
+  pixels.begin();
+  pixels.clear();
+  pixels.setBrightness(50); 
+  pinMode(INPUT_PIN, INPUT);
+
+  if (digitalRead(INPUT_PIN) == HIGH) {
+     // Set to Red
+    pixels.setPixelColor(0, pixels.Color(255, 0, 0)); 
+  } else {
+    
+    pixels.setPixelColor(0, pixels.Color(0, 0, 0));
+  }
+
   // ESP_ERROR_CHECK(twai_new_node_onchip(&node_config, &node_hdl));
   // ESP_ERROR_CHECK(twai_node_enable(node_hdl)); 
+  Serial.begin(9600);
 }
 
 void loop() {
   
-  pixels.setPixelColor(0, pixels.Color(255, 0, 0)); 
-  pixels.show(); // Send the updated color to the hardware
-  delay(500);
-
+  if (digitalRead(INPUT_PIN) == HIGH) {
+     
+    pixels.setPixelColor(0, pixels.Color(255, 0, 0)); 
+  } else {
+    
     pixels.setPixelColor(0, pixels.Color(0, 0, 0));
-  pixels.show();   
-  delay(500);
+  }
   
+  Serial0.write("Hello");
   
+
+ if (digitalRead(INPUT_PIN) == HIGH) {
+    pixels.setPixelColor(0, pixels.Color(255, 0, 0)); 
+  } else {
+    
+    pixels.setPixelColor(0, pixels.Color(0, 0, 0));
+  }
+   
+  if(Serial.available()) {
+    char input = Serial.read(); 
+Serial.println("I read" + input);
+} else {
+Serial.println("No data available"); 
+}
+  
+  pixels.show();  
+  
+    delay(500); 
 }
