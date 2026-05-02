77d47f4be8e518837a9f92b2676025551f649b8a
Ruthvik Venkatesan <ruthvikotravenkatesan@gmail.com>
2026-05-02 16:41:06 -0400

    led blink startup

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,33 @@
 #include <Arduino.h>
 #include <twai.h>
+#include <Adafruit_NeoPixel.h>
+
+// The built-in NeoPixel on the ESP32-C6-DevKitC-1 is on GPIO 8
+#define PIN        8 
+#define NUMPIXELS  1 
+
+// Set up the NeoPixel object
+Adafruit_NeoPixel pixels(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
 
 void setup() {
-  // put your setup code here, to run once:
-  ESP_ERROR_CHECK(twai_new_node_onchip(&node_config, &node_hdl));
-  ESP_ERROR_CHECK(twai_node_enable(node_hdl)); 
+  // --- NeoPixel Setup ---
+  pixels.begin(); // Initialize the NeoPixel strip object
+  pixels.clear(); // Set all pixel colors to 'off'
+  pixels.setBrightness(50); // Set brightness to ~20% (0-255) so it doesn't blind you
+  
+  // ESP_ERROR_CHECK(twai_new_node_onchip(&node_config, &node_hdl));
+  // ESP_ERROR_CHECK(twai_node_enable(node_hdl)); 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  
+  pixels.setPixelColor(0, pixels.Color(255, 0, 0)); 
+  pixels.show(); // Send the updated color to the hardware
+  delay(500);
+
+    pixels.setPixelColor(0, pixels.Color(0, 0, 0));
+  pixels.show();   
+  delay(500);
+  
+  
 }
